89fb75de203947fe2ad6213b07558049253ef751
kronk99 <92146862+kronk99@users.noreply.github.com>
2024-02-11 00:07:30 -0600

    Add files via upload
    Commit : elaboración inicial del problema 1, se trabajaron unas compuertas lógicas que permiten describir las 16 combinaciones posibles para un numero de 4 bits en binario a formato bcd, contiene el testbench y un pequeño programa con los 7 segmentos que permite visualizar el numero en BCD , se estiman cambios próximos , si la implementación es incorrecta

---
+++ Problema1/decodificador_tb.sv
@@ -0,0 +1,16 @@
+module decodificador_tb();
+	logic a,b,c,d,v,w,x,y,z;
+	decodificador decodificacion(a,b,c,d,v,w,x,y,z);
+	initial begin 
+	a=0;
+	b=0;
+	c=0;
+	d=0;
+	#40
+	a=1;
+	b=0;
+	c=1;
+	d=0;
+	end
+	
+endmodule
