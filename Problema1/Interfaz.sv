1da9caab1515dee7f9d432c74c5f96e23c450070
Luis S <ghaland550@gmail.com>
2024-02-15 23:36:26 -0600

    Commit, arreglo imparcial del codigo para el 7 segmentos
    La sintaxis necesaria para el 7 segmentos esta erronea, pero se desconoce la sintaxis correcta, por lo que es necesario ir a consulta para verificar el codigo y comprobar el correcto funcionamiento en la fpga

---
+++ Problema1/Interfaz.sv
@@ -0,0 +1,31 @@
+module Interfaz(input logic a, b, c, d, output logic [6:0] salida);
+  // Se instancian señales de salida del primer modulo
+  logic v, w, x, y, z;
+
+  // se intancian las entradas de entrada del primer modulo
+  logic [4:0] resultado;
+
+  // Instancia del primer módulo (decodificador)
+  Decodificador miDecodificador (
+    .a(a),
+    .b(b),
+    .c(c),
+    .d(d),
+    .v(v),
+    .w(w),
+    .x(x),
+    .y(y),
+    .z(z)
+  );
+
+  // Lógica para formar el array resultado
+  always_comb begin
+    resultado[4:0] = {v, w, x, y, z};  // Concatenación de v, w, x, y, z en resultado[4:0]
+  end
+
+  // Instancia del segundo módulo (segmentos)
+  Segmentos miSegmento (
+    .bcd(resultado),
+    .seg(salida)
+  );
+endmodule
--- Problema1/decodificador_tb.sv
+++ Problema1/decodificador_tb.sv
@@ -2,14 +2,52 @@ module decodificador_tb();
 	logic a,b,c,d,v,w,x,y,z;
 	decodificador decodificacion(a,b,c,d,v,w,x,y,z);
 	initial begin 
+	a = 5
 	a=0;
 	b=0;
 	c=0;
-	d=0;
+	d=1;
 	#40
-	a=1;
+	
+	a=0;
 	b=0;
 	c=1;
+	d=0;
+	#40
+	
+	a=0;
+	b=0;
+	c=1;
+	d=1;
+	#40
+	
+	a=0;
+	b=1;
+	c=0;
+	d=0;
+	#40
+	
+	a=0;
+	b=1;
+	c=0;
+	d=1;
+	#40
+	
+	a=0;
+	b=1;
+	c=1;
+	d=0;
+	#40
+	
+	a=0;
+	b=1;
+	c=1;
+	d=1;
+	#40
+	
+	a=1;
+	b=0;
+	c=0;
 	d=0;
 	end
 	
